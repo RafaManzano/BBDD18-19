15a589352969d1d1377b4157ad82128547743eb7
RafaManzano <rafamanzanomedina@gmail.com>
2018-11-08 14:35:52 +0100

    Subiendo ambos
    Relaxing acabados y urbanizacion empezando

---
+++ Ejercicio Urbanizacion.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE Urbanizacion
+
+GO 
+USE Urbanizacion
+GO
+
+CREATE TABLE Vehiculo (
+
+)
+
+CREATE TABLE Inquilino (
+
+)
+
+CREATE TABLE Vivienda (
+
+)
+
+CREATE TABLE Recibo (
+
+)
+
+CREATE TABLE Propietario (
+
+)
