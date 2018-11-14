1a64ddd1ec6aa115f5da723420d62b1c2d3bfff4
RafaManzano <rafamanzanomedina@gmail.com>
2018-11-14 22:49:16 +0100

    Subiendo check

--- RestriccionCK.sql
+++ RestriccionCK.sql
@@ -27,10 +27,26 @@ CREATE TABLE DatosRelacionados (
 NombreRelacionado CHAR (15) NULL,
 PalabraTabu CHAR (20) NULL,
 NumRarito TINYINT NULL,
+NumMasgrande SMALLINT NOT NULL,
+
 --PK,FK,UQ
 CONSTRAINT FK_DRelacionados_DRestrictivos FOREIGN KEY (NombreRelacionado) REFERENCES DatosRestrictivos(Nombre) ON DELETE NO ACTION ON UPDATE CASCADE,
+CONSTRAINT PK_DatosRelacionados PRIMARY KEY (NumMasgrande),
 
 --CHECK
 CONSTRAINT CK_PalabraTabu CHECK (PalabraTabu NOT IN ('Barcenas', 'Gurtel', 'Punica', 'Bankia', 'sobre') AND PalabraTabu NOT LIKE '%eo'),
+CONSTRAINT CK_NumRarito CHECK(NumRarito < 20 AND NumRarito % 2 = 0 OR NumRarito % 3 = 0 OR NumRarito % 5 = 0),
+CONSTRAINT CK_NumMasgrande CHECK (NumMasgrande BETWEEN NumRarito AND 1000),
+)
 
+CREATE TABLE DatosAlMogollon (
+ID SMALLINT NOT NULL,
+LimiteSuperior SMALLINT NULL,
+
+--PK,FK,UQ
+CONSTRAINT PK_DatosAlMogollon PRIMARY KEY (ID),
+
+--CHECK
+CONSTRAINT CK_ID CHECK (ID % 5 <> 0),
+CONSTRAINT CK_LimiteSuperior CHECK (LimiteSuperior BETWEEN 1500 AND 2000),
 )
