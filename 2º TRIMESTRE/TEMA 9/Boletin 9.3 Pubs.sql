86ddb9a74c2770eb175ef3d08b30cd7294c963d5
RafaManzano <rafamanzanomedina@gmail.com>
2019-02-04 21:45:08 +0100

    No acabado

--- 2º TRIMESTRE/TEMA 9/Boletin 9.3 Pubs.sql
+++ 2º TRIMESTRE/TEMA 9/Boletin 9.3 Pubs.sql
@@ -49,3 +49,24 @@ GROUP BY P.pub_id
 
 --7. Nombre, Apellidos y ciudad de todos los autores que han trabajado para la editorial 
 --"Binnet & Hardley” o "Five Lakes Publishing”
+SELECT A.au_fname, A.au_lname, A.city FROM authors AS A
+INNER JOIN titleauthor AS TA ON TA.au_id = A.au_id
+INNER JOIN titles AS T ON T.title_id = TA.title_id
+INNER JOIN publishers AS P ON P.pub_id = T.pub_id
+WHERE P.pub_name IN('Binnet & Hardley','Five Lakes Publishing')
+
+--8. Empleados que hayan trabajado en alguna editorial que haya publicado algún libro 
+--en el que alguno de los autores fuera Marjorie Green o Michael O'Leary.
+SELECT DISTINCT E.fname,E.lname FROM employee AS E
+INNER JOIN publishers AS P ON P.pub_id = E.pub_id
+INNER JOIN titles AS T ON T.pub_id = P.pub_id
+INNER JOIN titleauthor AS TA ON TA.title_id = T.title_id
+INNER JOIN authors AS A ON A.au_id = TA.au_id
+WHERE (A.au_fname = 'Marjorie' OR A.au_fname = 'Michael') AND (A.au_lname = 'Green' OR A.au_lname = 'O''Leary')
+
+--9. Número de ejemplares vendidos de cada libro, especificando el título y el tipo. No entiendo porque no sale nada
+SELECT SUM(S.qty) AS Quantity, T.title_id, T.title, T.type FROM titles AS T
+INNER JOIN sales AS S ON S.title_id = T.title
+GROUP BY T.title_id, T.title, T.type
+
+--10. Número de ejemplares de todos sus libros que ha vendido cada autor.
