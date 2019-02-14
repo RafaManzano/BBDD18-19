9939139dab7eef67dc70b4ab3d8885ed0f41f3ff
RafaManzano <rafamanzanomedina@gmail.com>
2019-02-14 10:10:31 +0100

    Comprobar 8

--- 2º TRIMESTRE/TEMA 9/Boletin 9.5 LeoMetro.sql
+++ 2º TRIMESTRE/TEMA 9/Boletin 9.5 LeoMetro.sql
@@ -1,7 +1,7 @@
 USE LeoMetroV2
 SELECT * FROM LM_Estaciones
 SELECT * FROM LM_Lineas
-
+SELECT * FROM LM_Viajes
 --1. Indica el número de estaciones por las que pasa cada línea
 SELECT COUNT(DISTINCT LMR.estacion) AS NumeroEstaciones, LML.ID FROM LM_Estaciones AS LME
 INNER JOIN LM_Recorridos AS LMR ON LMR.estacion = LME.ID
@@ -53,3 +53,20 @@ GROUP BY LMR.Tren
 --7. Calcula cuál ha sido el intervalo de tiempo en que más personas registradas han estado en el metro 
 --al mismo tiempo. Considera intervalos de una hora (de 12:00 a 12:59, de 13:00 a 13:59, etc). 
 --Si hay varios momentos con el número máximo de personas, muestra el más reciente.
+
+--8 Calcula el dinero gastado por cada usuario en el mes de febrero de 2017. El precio de un viaje es el de la zona más 
+--cara que incluya. Incluye a los que no han viajado.
+SELECT * FROM LM_Pasajeros
+
+--Creo que no es corercto, de toda formas preguntar a Leo
+--Se suma para saber lo que se gasta cada persona en el mes
+SELECT SUM(LMZP.Precio_Zona + LMV.Importe_Viaje) AS GastosMes, LMP.ID FROM LM_Estaciones AS LME
+INNER JOIN LM_Zona_Precios AS LMZP ON LMZP.Zona = LME.Zona_Estacion
+INNER JOIN LM_Viajes AS LMV ON LME.ID = LMV.IDEstacionEntrada OR LME.ID = LMV.IDEstacionSalida
+INNER JOIN LM_Tarjetas AS LMT ON LMV.IDTarjeta = LMT.ID
+INNER JOIN LM_Pasajeros AS LMP ON LMP.ID = LMT.IDPasajero
+WHERE MONTH(LMV.MomentoEntrada) = 2 AND MONTH(LMV.MomentoSalida) = 2 AND YEAR(LMV.MomentoEntrada) = 2017 AND YEAR(LMV.MomentoSalida) = 2017
+GROUP BY LMP.ID
+
+SELECT LMZP.Precio_Zona, LME.ID FROM LM_Estaciones AS LME
+INNER JOIN LM_Zona_Precios AS LMZP ON LMZP.Zona = LME.Zona_Estacion
