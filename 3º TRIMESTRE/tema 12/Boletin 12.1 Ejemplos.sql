94f1ec3e9d05f8a0e4a26454563f356d320c0b71
Rafael Manzano <rafamanzanomedina@gmail.com>
2019-05-14 11:02:54 +0200

    El 6

---
+++ 3º TRIMESTRE/tema 12/Boletin 12.1 Ejemplos.sql
@@ -0,0 +1,146 @@
+USE Ejemplos
+GO
+CREATE TABLE Palabras (
+ID SmallInt Not Null Identity Constraint PK_Palabras Primary Key
+,Palabra VarChar(30) Null
+)
+
+--1. Queremos que cada vez que se actualice la tabla Palabras aparezca un mensaje diciendo si
+--se han añadido, borrado o actualizado filas.
+--Pista: Crea tres triggers diferentes
+GO
+CREATE TRIGGER Insertado ON Palabras
+	AFTER INSERT AS
+	BEGIN 
+		PRINT 'Se ha insertado correctamente'
+	END
+GO
+
+GO
+CREATE TRIGGER Eliminado ON Palabras
+	AFTER DELETE AS
+	BEGIN 
+		PRINT 'Se ha eliminado correctamente'
+	END
+GO
+
+GO
+CREATE TRIGGER Actualizacion ON Palabras
+	AFTER UPDATE AS
+	BEGIN 
+		PRINT 'Se ha actualizado correctamente'
+	END
+GO
+
+--Pruebas
+INSERT Palabras (Palabra)
+VALUES ('Futbol'), ('Baloncesto')
+
+DELETE Palabras
+WHERE Palabra = 'Futbol'
+
+UPDATE Palabras
+SET Palabra = 'Balonmano'
+WHERE Palabra = 'Baloncesto'
+
+SELECT * FROM Palabras
+
+--2. Haz un trigger que cada vez que se aumente o disminuya el número de filas de la tabla
+--Palabras nos diga cuántas filas hay.
+GO
+CREATE TRIGGER ContarFilas ON Palabras
+	AFTER INSERT, DELETE AS
+	BEGIN 
+	DECLARE @Filas INT
+		SET @Filas = (SELECT COUNT(*) FROM Palabras)
+		PRINT CONCAT( 'Hay ', @Filas ,' filas')
+	END
+GO
+
+--Pruebas
+INSERT Palabras (Palabra)
+VALUES ('Futbol'), ('Baloncesto')
+
+DELETE Palabras
+WHERE Palabra = 'Futbol'
+
+--3. Cada vez que se inserte una fila queremos que se muestre un mensaje indicando
+--“Insertada la palabra ________”
+GO
+CREATE TRIGGER FilaIntroducida ON Palabras
+	AFTER INSERT AS
+	BEGIN
+		DECLARE @IDMaximo INT 
+		SET @IDMaximo = (SELECT MAX(ID) FROM Palabras) 
+		DECLARE @Nombre VARCHAR(30)
+		SET @Nombre = (SELECT Palabra FROM Palabras WHERE ID = @IDMaximo)
+		PRINT CONCAT( 'Insertada la palabra ', @Nombre)
+	END
+GO
+
+--Pruebas
+INSERT Palabras (Palabra)
+VALUES ('Waterpolo')
+
+INSERT Palabras (Palabra)
+VALUES ('BalonVolea')
+
+INSERT Palabras (Palabra)
+VALUES ('Futbol')
+
+SELECT * FROM Palabras
+
+--4. Cada vez que se inserten filas que nos diga “XX filas insertadas”
+GO
+CREATE TRIGGER ContarFilasInsertadas ON Palabras
+	AFTER INSERT AS
+	BEGIN 
+	DECLARE @Filas INT
+		SET @Filas = (SELECT COUNT(*) FROM inserted)
+		PRINT CONCAT( @Filas ,' filas insertadas')
+	END
+GO
+
+--Pruebas
+INSERT Palabras (Palabra)
+VALUES ('Futbol'), ('Baloncesto')
+
+
+INSERT Palabras (Palabra)
+VALUES ('Futbol'), ('Baloncesto'), ('Sumo')
+
+--5. Que no permita introducir palabras repetidas (sin usar UNIQUE).
+GO
+CREATE TRIGGER RestringirNombreRepetidos ON Palabras
+	AFTER INSERT AS
+	BEGIN
+		DECLARE @IDMaximo INT 
+		SET @IDMaximo = (SELECT MAX(ID) FROM Palabras) 
+		DECLARE @Insertado VARCHAR(30)
+		SET @Insertado = (SELECT Palabra FROM inserted WHERE ID = @IDMaximo) 
+		IF EXISTS (SELECT COUNT(ID) FROM Palabras WHERE Palabra = @Insertado HAVING COUNT(ID) > 1)
+			BEGIN
+				ROLLBACK
+			END
+	END
+GO
+
+--Pruebas
+INSERT Palabras (Palabra)
+VALUES ('Futbol')
+
+INSERT Palabras (Palabra)
+VALUES ('Parapente')
+
+INSERT Palabras (Palabra)
+VALUES ('Ala Delta')
+
+INSERT Palabras (Palabra)
+VALUES ('Rafting')
+
+SELECT * FROM Palabras
+
+--BBDD LeoMetro
+USE LeoMetroV2
+--6. Comprueba que un pasajero no pueda entrar o salir por la misma estación más de tres
+--veces el mismo día
